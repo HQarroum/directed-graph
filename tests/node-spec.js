9bb088e5c0a95606602bee0047fd67aeda7df8d8
Halim Qarroum <hqm.post@gmail.com>
2017-06-06 21:16:42 +0200

    Migrated tests to mocha + integrated codecov

--- tests/node-spec.js
+++ tests/node-spec.js
@@ -1,72 +1,72 @@
-define(['lodash', 'graph'], function (_, Graph) {
+var _ = require('lodash');
+var expect = require('expect');
+var Graph = require('../graph.js');
+
+/**
+ *  Node test plan.
+ */
+describe('A node implementation', function () {
+
+    var graph = new Graph();
 
     /**
-     *  Node test plan.
+     * Building the graph using a predefined path :
+     *
+     *                             Head
+     *                              ||
+     *                            /   \
+     *                           \/   \/
+     *                          foo   bar
+     *                          ||    ||
+     *                           \    /
+     *                           \   /
+     *                            \/
+     *                           baz
      */
-    describe('A node implementation', function () {
+    beforeEach(function () {
+        graph.addEdge('head', 'foo');
+        graph.addEdge('head', 'bar');
+        graph.addEdge('foo', 'baz');
+        graph.addEdge('bar', 'baz');
+    });
 
-        var graph = new Graph();
+    /**
+     * Retrieving a node parents.
+     */
+    it('should be able to return its parents', function () {
+        var head = graph.nodes['head'];
+        var foo  = graph.nodes['foo'];
+        var baz  = graph.nodes['baz'];
 
-        /**
-         * Building the graph using a predefined path :
-         *
-         *                             Head
-         *                              ||
-         *                            /   \
-         *                           \/   \/
-         *                          foo   bar
-         *                          ||    ||
-         *                           \    /
-         *                           \   /
-         *                            \/
-         *                           baz
-         */
-        beforeEach(function () {
-            graph.addEdge('head', 'foo');
-            graph.addEdge('head', 'bar');
-            graph.addEdge('foo', 'baz');
-            graph.addEdge('bar', 'baz');
-        });
+        expect(head.parents().length).toEqual(0);
+        expect(_.map(foo.parents(), 'id')).toEqual(['head']);
+        expect(_.map(baz.parents(), 'id')).toEqual(['foo', 'bar']);
+    });
 
-        /**
-         * Retrieving a node parents.
-         */
-        it('should be able to return its parents', function () {
-            var head = graph.nodes['head'];
-            var foo  = graph.nodes['foo'];
-            var baz  = graph.nodes['baz'];
+    /**
+     * Retrieving a node children.
+     */
+    it('should be able to return its children', function () {
+        var head = graph.nodes['head'];
+        var foo  = graph.nodes['foo'];
+        var baz  = graph.nodes['baz'];
 
-            expect(head.parents().length).toEqual(0);
-            expect(_.map(foo.parents(), 'id')).toEqual(['head']);
-            expect(_.map(baz.parents(), 'id')).toEqual(['foo', 'bar']);
-        });
+        expect(_.map(head.children(), 'id')).toEqual(['foo', 'bar']);
+        expect(_.map(foo.children(), 'id')).toEqual(['baz']);
+        expect(baz.children().length).toEqual(0);
+    });
 
-        /**
-         * Retrieving a node children.
-         */
-        it('should be able to return its children', function () {
-            var head = graph.nodes['head'];
-            var foo  = graph.nodes['foo'];
-            var baz  = graph.nodes['baz'];
+    /**
+     * Comparing two nodes.
+     */
+    it('should be able to provide a comparable interface', function () {
+        var head = graph.nodes['head'];
+        var foo  = graph.nodes['foo'];
+        var baz  = graph.nodes['baz'];
 
-            expect(_.map(head.children(), 'id')).toEqual(['foo', 'bar']);
-            expect(_.map(foo.children(), 'id')).toEqual(['baz']);
-            expect(baz.children().length).toEqual(0);
-        });
-
-        /**
-         * Comparing two nodes.
-         */
-        it('should be able to provide a comparable interface', function () {
-            var head = graph.nodes['head'];
-            var foo  = graph.nodes['foo'];
-            var baz  = graph.nodes['baz'];
-
-            expect(head.equals(head)).toBeTruthy();
-            expect(head.equals(foo)).toBeFalsy();
-            expect(head.equals(baz)).toBeFalsy();
-        });
-
+        expect(head.equals(head)).toBeTruthy();
+        expect(head.equals(foo)).toBeFalsy();
+        expect(head.equals(baz)).toBeFalsy();
     });
 
 });
--- tests/route-management-spec.js
+++ tests/route-management-spec.js
@@ -1,116 +1,115 @@
-define(['lodash', 'graph'], function (_, Graph) {
+var _ = require('lodash');
+var expect = require('expect');
+var Graph = require('../graph.js');
+
+/**
+ *  Route management test plan.
+ */
+describe('Graph routes APIs', function () {
+
+    var graph = new Graph();
 
     /**
-     *  Route management test plan.
+     * Helper function that will map an array
+     * of routes to an aray of array of node identifiers.
      */
-    describe('Graph routes APIs', function () {
+    var routesToArray = function (routes) {
+        var input = [];
 
-        var graph = new Graph();
-
-        /**
-         * Helper function that will map an array
-         * of routes to an aray of array of node identifiers.
-         */
-        var routesToArray = function (routes) {
-            var input = [];
-
-            _.each(routes, function (route) {
-                var map = _.map(route.path, 'id');
-                input.push({ path: map, weight: route.weight });
-            });
-            return input;
-        };
-
-        /**
-         * Building the graph using a predefined path :
-         *
-         *                             Head
-         *                              ||
-         *                            /   \
-         *                           \/   \/
-         *                          foo   bar
-         *                          ||    ||
-         *                           \    /
-         *                           \   /
-         *                            \/
-         *                           baz
-         */
-        beforeEach(function () {
-            graph.addEdge('head', 'foo', { weight: 1 });
-            graph.addEdge('head', 'bar', { weight: 1 });
-            graph.addEdge('foo', 'baz', { weight: 2 });
-            graph.addEdge('bar', 'baz', { weight: 2 });
+        _.each(routes, function (route) {
+            var map = _.map(route.path, 'id');
+            input.push({ path: map, weight: route.weight });
         });
+        return input;
+    };
 
-        it('should be able to return all the routes having a node `n` as the head', function () {
-            var result_head = [
-                { path: ['head', 'foo'], weight: 1 },
-                { path: ['head', 'foo', 'baz'], weight: 3 },
-                { path: ['head', 'bar'], weight: 1 },
-                { path: ['head', 'bar', 'baz'], weight: 3 }
-            ];
-
-            var result_foo = [
-                { path: ['foo', 'baz'], weight: 2 }
-            ];
-
-            var output_head = routesToArray(graph.routes({ from: 'head' }));
-            var output_foo  = routesToArray(graph.routes({ from: 'foo' }));
-
-            expect(JSON.stringify(result_head)).toEqual(JSON.stringify(output_head));
-            expect(JSON.stringify(result_foo)).toEqual(JSON.stringify(output_foo));
-        });
-
-        it('should be able to return all the routes having a node `n` as the head using additional query parameters', function () {
-            var result = [
-                { path: ['head', 'foo', 'baz'], weight: 3 },
-                { path: ['head', 'bar', 'baz'], weight: 3 }
-            ];
-
-            var output = routesToArray(graph.routes({
-                from: 'head',
-                where: {
-                    length: 3
-                }
-            }));
-
-            expect(JSON.stringify(result)).toEqual(JSON.stringify(output));
-        });
-
-        it('should be able to find a route given an array of node identifiers', function () {
-            // Searching for the route head->foo->baz
-            var route = graph.findRoute(['head', 'foo', 'baz']);
-            expect(route).not.toBeUndefined();
-            expect(route.weight).toEqual(3);
-            expect(route.path.length).toEqual(3);
-
-            // Searching for the route foo->baz
-            route = graph.findRoute(['foo', 'baz']);
-            expect(route).not.toBeUndefined();
-            expect(route.weight).toEqual(2);
-            expect(route.path.length).toEqual(2);
-
-            // Searching for an invalid route
-            route = graph.findRoute(['baz']);
-            expect(route).toBeUndefined();
-        });
-
-        it('should be able to state whether a route exists in the graph', function () {
-            // Retrieving all the routes existing in the graph
-            // which have `head` as the head node.
-            var routes = graph.routes({ from: 'head' });
-
-            _.each(routes, function (route) {
-                expect(graph.hasRoute(route)).toBeTruthy();
-            });
-            graph.clear();
-            // Verifying that the `hasRoute` method returns
-            // false if the routes do not exist anymore.
-            _.each(routes, function (route) {
-                expect(graph.hasRoute(route)).toBeFalsy();
-            });
-        });
-
+    /**
+     * Building the graph using a predefined path :
+     *
+     *                             Head
+     *                              ||
+     *                            /   \
+     *                           \/   \/
+     *                          foo   bar
+     *                          ||    ||
+     *                           \    /
+     *                           \   /
+     *                            \/
+     *                           baz
+     */
+    beforeEach(function () {
+        graph.addEdge('head', 'foo', { weight: 1 });
+        graph.addEdge('head', 'bar', { weight: 1 });
+        graph.addEdge('foo', 'baz', { weight: 2 });
+        graph.addEdge('bar', 'baz', { weight: 2 });
     });
 
+    it('should be able to return all the routes having a node `n` as the head', function () {
+        var result_head = [
+            { path: ['head', 'foo'], weight: 1 },
+            { path: ['head', 'foo', 'baz'], weight: 3 },
+            { path: ['head', 'bar'], weight: 1 },
+            { path: ['head', 'bar', 'baz'], weight: 3 }
+        ];
+
+        var result_foo = [
+            { path: ['foo', 'baz'], weight: 2 }
+        ];
+
+        var output_head = routesToArray(graph.routes({ from: 'head' }));
+        var output_foo  = routesToArray(graph.routes({ from: 'foo' }));
+
+        expect(JSON.stringify(result_head)).toEqual(JSON.stringify(output_head));
+        expect(JSON.stringify(result_foo)).toEqual(JSON.stringify(output_foo));
+    });
+
+    it('should be able to return all the routes having a node `n` as the head using additional query parameters', function () {
+        var result = [
+            { path: ['head', 'foo', 'baz'], weight: 3 },
+            { path: ['head', 'bar', 'baz'], weight: 3 }
+        ];
+
+        var output = routesToArray(graph.routes({
+            from: 'head',
+            where: {
+                length: 3
+            }
+        }));
+
+        expect(JSON.stringify(result)).toEqual(JSON.stringify(output));
+    });
+
+    it('should be able to find a route given an array of node identifiers', function () {
+        // Searching for the route head->foo->baz
+        var route = graph.findRoute(['head', 'foo', 'baz']);
+        expect(route).toNotBe(undefined);
+        expect(route.weight).toEqual(3);
+        expect(route.path.length).toEqual(3);
+
+        // Searching for the route foo->baz
+        route = graph.findRoute(['foo', 'baz']);
+        expect(route).toNotBe(undefined);
+        expect(route.weight).toEqual(2);
+        expect(route.path.length).toEqual(2);
+
+        // Searching for an invalid route
+        route = graph.findRoute(['baz']);
+        expect(route).toBe(undefined);
+    });
+
+    it('should be able to state whether a route exists in the graph', function () {
+        // Retrieving all the routes existing in the graph
+        // which have `head` as the head node.
+        var routes = graph.routes({ from: 'head' });
+
+        _.each(routes, function (route) {
+            expect(graph.hasRoute(route)).toBeTruthy();
+        });
+        graph.clear();
+        // Verifying that the `hasRoute` method returns
+        // false if the routes do not exist anymore.
+        _.each(routes, function (route) {
+            expect(graph.hasRoute(route)).toBeFalsy();
+        });
+    });
 });
